1286e9045026d2d0c8f21348d6bb660dce142113
Sajeth <sajethbabu@gmail.com>
2023-07-29 16:05:49 -0300

    fix: initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
 module stocks.commons {
     requires spring.web;
-    exports com.saji.stocks.pojo.selenium to stocks.selenium.connector;
+    exports com.saji.stocks.pojo.selenium to stocks.selenium.connector,stocks.listener.services;
 }
